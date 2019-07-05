18725a06ac1d302fcd12bddfa7c1717ed92f4185
Pantelis Goumas <pgoumas@hotmail.com>
2019-07-05 16:49:53 +0200

    new commit

---
+++ my-app/config.js
@@ -0,0 +1,8 @@
+const config = {
+    apiKey: "AIzaSyDTl4HMEgs34c4YRDgE6QXVOPCZB3vd738",
+    authDomain: "pg-react-01.firebaseapp.com",
+    databaseURL: "https://pg-react-01.firebaseio.com"
+  };
+  
+  export default config;
+  
